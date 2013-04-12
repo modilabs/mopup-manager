fcfeeb63d5d8a1069606abf192c7ff866c3aafc9
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-04-12 12:11:34 -0400

    example angular project (tabs)

---
+++ example.js
@@ -0,0 +1,7 @@
+angular.module('plunker', ['ui.bootstrap']);
+var TabsDemoCtrl = function ($scope) {
+  $scope.panes = [
+    { title:"Dynamic Title 1", content:"Dynamic content 1" },
+    { title:"Dynamic Title 2", content:"Dynamic content 2" }
+  ];
+};
