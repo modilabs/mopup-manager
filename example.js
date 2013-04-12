53adb2d14dd9148973fca8490dc7653bde9001d2
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-04-12 12:25:04 -0400

    light additions

--- example.js
+++ example.js
@@ -1,7 +1,12 @@
 angular.module('plunker', ['ui.bootstrap']);
 var TabsDemoCtrl = function ($scope) {
   $scope.panes = [
-    { title:"Dynamic Title 1", content:"Dynamic content 1" },
-    { title:"Dynamic Title 2", content:"Dynamic content 2" }
+    { title: "Health Facilities", content: "Health Facilities will be here", active: true},
+    { title: "Education Facilities", content: "Education Facilities will be here"}
+  ];
+};
+
+var FaciltiesListCtrl = function ($scope) {
+  $scope.facilities = [
   ];
 };
