42f29c03a1e62e75f64e89ca526d91c4a2c2c08c
modilabs-starscream <modilabs.starscream@gmail.com>
2013-04-12 13:01:58 -0400

    some progress based on static data

--- example.js
+++ example.js
@@ -8,5 +8,38 @@ var TabsDemoCtrl = function ($scope) {
 
 var FaciltiesListCtrl = function ($scope) {
   $scope.facilities = [
+  { 'facility_name' : 'Uratta Health Centre',
+    'state' : 'abia',
+    'lga': 'aba_north',
+    'ward': 'Uratta',
+    'community': 'Umughbuji',
+    'facility_type': 'primaryhealthcarecentre'
+  }, {
+    'facility_name' : 'Asaokpuaja Health Centre',
+    'facility_type' : 'healthpostdispensary',
+    'state' : 'abia',
+    'lga': 'aba_north',
+    'ward': 'Asaokpuaja',
+    'community': 'Asaokpuaja',
+  }
+  ];
+};
+
+var NMISListCtrl = function ($scope) {
+  $scope.facilities = [
+  { 'facility_name' : 'Asa Okpulor Health Centre',
+    'state' : 'abia',
+    'lga': 'aba_north',
+    'ward': 'Uratta',
+    'community': 'Uratta',
+    'facility_type': 'primaryhealthcarecentre'
+  }, {
+    'facility_name' : 'Asa Ukonu Health Centre',
+    'facility_type' : 'primaryhealthcarecentre',
+    'state' : 'abia',
+    'lga': 'aba_north',
+    'ward': 'Asa',
+    'community': 'Asa',
+  }
   ];
 };
