57ecd4fc84a468107bfbded934ecdc2f58850883
Myf Ma <myf@worldtalks.org>
2013-04-19 16:04:28 -0400

    resize on jquery ready

--- front.js
+++ front.js
@@ -1,5 +1,10 @@
 //resize page to fit vertical scroll
-$(window).on('resize', function(){
+var onResize = function(){
   var h = $(window).height() - 128;
   $('#nmis_list').css({height: h});
+};
+$(window).on('resize', onResize);
+$(function(){
+  $(window).trigger('resize');
 });
+
