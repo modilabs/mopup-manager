b0b110d09d76c32b2917e5c226ee857bf529836d
Prajjwol Gautam <prajjwol@gmail.com>
2013-04-19 15:54:49 -0400

    scroll bar

--- front.js
+++ front.js
@@ -1,5 +1,5 @@
 //resize page to fit vertical scroll
-$(window).on('resize', function(){ 
-  var h = $(window).height()-128; 
+$(window).on('resize', function(){
+  var h = $(window).height() - 128;
   $('#nmis_list').css({height: h});
 });
